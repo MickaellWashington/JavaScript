87015791e5397828f6a07119d6cf9bad52fb665a
Mickaell Washington <mickaell.washington@hotmail.com>
2023-01-25 21:19:05 -0300

    while e do while

---
+++ Aula13/ambiente.js
@@ -0,0 +1,14 @@
+var c = 1
+while (c <= 5) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+// While: Estrutura de repetição com teste lógico no início
+
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+// do while Estrutura de repetição com teste lógico no final 
