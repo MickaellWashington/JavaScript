e6f9c0074d6cb27632bfb0e4d1c82597c948ee0a
Mickaell Washington <mickaell.washington@hotmail.com>
2023-01-29 19:59:11 -0300

    Update script.js

--- Aula14/ex016/script.js
+++ Aula14/ex016/script.js
@@ -1,18 +1,48 @@
 function contar() {
+
     let ini = document.querySelector('#txti')
     let fim = document.querySelector('#txtf')
     let pss = document.querySelector('#txtp')
     let res = document.querySelector('#res')
+
     if (ini.value.length == 0 || fim.value.length == 0 || pss.value.length == 0) {
-        alert('[ERRO404] Preencha os dados e tente novamente!')
+
+        res.innerHTML = 'Impossível contar!'
+
     } else {
+
         res.innerHTML = 'Contando: '
+
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(pss.value)
 
-        for (let c = i; c <= f; c += p) {
-            res.innerHTML += `${c} `
+        if (p <= 0) {
+
+            alert('Passo inválido, considerando Passo 1')
+            p = 1
+
         }
-    }
+
+        if (i < f) {
+
+//contagem crescente:
+
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} `
+                }
+
+     } else {
+
+//contagem decrescente
+
+        for (let c = i; c >= f; c -= p) {
+            res.innerHTML += `${c} `
+        } 
+      
+     }
+
+     res.innerHTML += `\u{1f3c1}`
+     
+  }
 }
