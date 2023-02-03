275ffa3eab71344a2189b82692605e0fa0f0a864
Mickaell Washington <mickaell.washington@hotmail.com>
2023-02-03 12:16:28 -0300

    ExerciciosDaNet

---
+++ ExerciciosDaNet/ex001/ex01/maiorQue.js
@@ -0,0 +1,7 @@
+    let num1 = 4
+    let num2 = 5
+    if (num1 > num2) {
+            console.log('Maior')
+        } else {
+            console.log('Menor')
+        }
---
+++ ExerciciosDaNet/ex001/ex02/votaNaoVota.js
@@ -0,0 +1,11 @@
+let idade = 75
+
+if (idade < 16) {
+    console.log('Não vota')
+} else if (idade >= 16 && idade < 18) {
+    console.log('Voto, opcional')
+}  else if (idade >= 75) {
+    console.log('Voto, opcional')
+} else {
+    console.log('Voto, obrigatório')
+}
